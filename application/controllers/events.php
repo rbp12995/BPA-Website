2d81a8edb01b27fcecd65b7a03eafdcb957bf7b9
unknown <4201@.(none)>
2011-09-15 09:31:49 -0400

    Added "events" controller.

---
+++ application/controllers/events.php
@@ -0,0 +1,10 @@
+<?
+class Events extends CI_Controller {
+public function index(){
+	$options = array();
+	$options['signedin'] = FALSE;
+	$events = $this->db->get('events')->result_array();
+	$options['events'] = $events;
+	$this->load->view('events');
+}
+}
