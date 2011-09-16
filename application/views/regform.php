f54f147e07bd46ffa295e7d5e98331d4a5aefeea
akuklev <andrey.kuklev@gmail.com>
2011-09-15 23:02:20 -0300

    Edited application/views/regform.php via GitHub

--- application/views/regform.php
+++ application/views/regform.php
@@ -42,7 +42,7 @@
 
 <h1> Register Now! </h1>
 <h2> For the 2011-2012 Huron Chapter...</h2>
-<form target="http://huronbpa.org/register/submit" method="POST">
+<form action="http://huronbpa.org/register/submit" method="POST">
 <p><?
 	// Errors with form validation will be displayed here.
 	echo validation_errors();
